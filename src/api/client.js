d82b34e5478b077bcfd2da24b7ecdfea9d11ee17
Maria Iudina <gretarichter@gmail.com>
2025-12-05 11:18:01 +0200

    Add SPA rewrite for Vercel

--- src/api/client.js
+++ src/api/client.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json"
-  }
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export function setAdminToken(token) {
